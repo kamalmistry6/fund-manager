b3f7865ddfa2a7ccd24926be433df9b806fb76cd
Kamal Mistry <131192608+kamalmistry6@users.noreply.github.com>
2025-07-11 17:15:27 +0530

    Update fund.service.ts

--- src/app/admin/Services/fund.service.ts
+++ src/app/admin/Services/fund.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { fund } from '../models/funds';
 import { environment } from '../../../environments/environment';
